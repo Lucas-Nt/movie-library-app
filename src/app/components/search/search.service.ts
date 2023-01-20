48d6e62fbafd9ffeab02b072d140bf2dd7616cd4
Loukas Ntaopoulos <lntaopoulos@gmail.com>
2023-01-20 11:29:05 +0200

    angular material update

--- src/app/components/search/search.service.ts
+++ src/app/components/search/search.service.ts
@@ -1,35 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from '@angular/core'
 
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs'
 
-import { SearchType } from 'src/app/shared/enums/search-type.enum';
-import { SearchParams } from './search-movies/search-movies.component';
+import { SearchType } from 'src/app/shared/enums/search-type.enum'
+import { SearchParams } from './search-movies/search-movies.component'
 
 @Injectable()
 export class SearchService {
-
-  public lastMovieSearchParams: SearchParams;
-  public movieTvShowResults = new BehaviorSubject(null);
+  public lastMovieSearchParams: SearchParams
+  public movieTvShowResults = new BehaviorSubject(null)
 
   private _defaultSearchParams: SearchParams = {
     title: '',
-    searchType: SearchType.MOVIE
-  };
+    searchType: SearchType.MOVIE,
+  }
 
   public evaluateDataClearing(url: string): void {
-    const isInSearchOrDetailView = url.includes('search') ||
-                                   url.includes('details');
+    const isInSearchOrDetailView = url.includes('search') || url.includes('details')
 
     if (!isInSearchOrDetailView) {
-      this.resetParams();
-      this.movieTvShowResults.next(null);
+      this.resetParams()
+      this.movieTvShowResults.next(null)
     }
   }
 
   private resetParams(): void {
     this.lastMovieSearchParams = {
-      ...this._defaultSearchParams
-    };
+      ...this._defaultSearchParams,
+    }
   }
-
 }
