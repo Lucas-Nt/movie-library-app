740d2c226f803688a863745d8f887d954ac02ade
Loukas Ntaopoulos <lntaopoulos@gmail.com>
2024-02-27 16:22:42 +0200

    rating value fix

--- src/app/shared/components/rating-circle/rating-circle.component.ts
+++ src/app/shared/components/rating-circle/rating-circle.component.ts
@@ -1,23 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from '@angular/core'
 
 @Component({
   selector: 'app-rating-circle',
   templateUrl: './rating-circle.component.html',
-  styleUrls: ['./rating-circle.component.scss']
+  styleUrls: ['./rating-circle.component.scss'],
 })
 export class RatingCircleComponent {
+  @Input() customClass: string
 
-  @Input() customClass: string;
-
-  @Input() set rating(number: any) {
-    const decimalNumber = number / 10;
-    this.ratingScore = number;
-    this.strokeOffset =  this.circumference * (1 - decimalNumber);
+  @Input() set rating(value: number) {
+    const decimalNumber = value / 10
+    this.ratingScore = +value.toFixed(1)
+    this.strokeOffset = this.circumference * (1 - decimalNumber)
   }
 
-  public circleRadius = 40;
-  public circumference =  2 * Math.PI * this.circleRadius;
-  public ratingScore: number;
-  public strokeOffset: number;
-
+  public circleRadius = 40
+  public circumference = 2 * Math.PI * this.circleRadius
+  public ratingScore: number
+  public strokeOffset: number
 }
